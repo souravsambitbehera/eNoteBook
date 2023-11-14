d344c69d3e5ee2b33740ff94f1777dd8370411ae
sourav sambit behera <souravsambit12@gmail.com>
2023-11-14 17:42:15 +0530

    creatings models

---
+++ eNoteBook/backend/models/Models.js
@@ -0,0 +1,22 @@
+import mongoose, { model } from 'mongoose';
+const { Schema } = mongoose;
+
+const NotesSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    tag: {
+        type: String,
+        default: "General"
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+});
+export default model('notes', NotesSchema);
