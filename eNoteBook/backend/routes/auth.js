c7bf673cae94d4e8af77f536cb0131de50a4598c
sourav sambit behera <souravsambit12@gmail.com>
2023-11-15 11:05:37 +0530

    create signup api

--- eNoteBook/backend/routes/auth.js
+++ eNoteBook/backend/routes/auth.js
@@ -2,11 +2,41 @@ import express from "express"
 import User from "../models/User.js"
 const router = express.Router() //importing express router
 
-router.post("/",(req,res)=>{ //it will render on specific routes
-    console.log(req.body)
-    const user = User(req.body)
-    user.save()
-    res.send(req.body)
+router.post("/signup", async(req,res)=>{ //it will render on specific routes
+  const {name,email,password}= req.body //data coming from frontend // and i'm destructure thr req.body 
+  try {
+    if(!name || !email || !password){
+        //error will be shown as json if any one of the field is empty
+        return res.status(400).json({error:"All fields are required"})
+
+    }
+    if(!email.includes("@")){
+        //if email does not include @ in email field
+        return res.status(400).json({error:"Please enter a valid Email"})
+    }
+
+    const user =await User.findOne({email}) //find user with unique email
+    if(user){
+        return res.status(400).json({error:"user already exit, Enter a new Email"})
+    }
+
+    //save data into db
+    const newUser = await User({
+        name,
+        email,
+        password,
+    })
+    await newUser.save()
+    console.log(newUser)
+    res.status(201).json({success:"Signup Successfully"})
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).send("Internal Server Error")
+    
+  }
+
+
   
 })
 // router.get("/",(req,res)=>{
